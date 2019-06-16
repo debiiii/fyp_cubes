20a74af47d1b9bc48ff8302ec70cf52845e633eb
debiiii <debbiewong22@gmail.com>
2019-06-17 00:43:50 +0800

    20190616_2010_DectionCheckBugDoneHvSkip

---
+++ androidTest/app/src/main/java/ytwong239/scm/cubicdebug/OpenGLSurfaceView_3DModel.java
@@ -0,0 +1,19 @@
+package ytwong239.scm.cubicdebug;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+/**
+ * Created by DebbieWong on 14/3/2019.
+ */
+
+public class OpenGLSurfaceView_3DModel extends GLSurfaceView {
+    public OpenGLSurfaceView_3DModel(Context context) {
+        super(context);
+    }
+
+    public OpenGLSurfaceView_3DModel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
